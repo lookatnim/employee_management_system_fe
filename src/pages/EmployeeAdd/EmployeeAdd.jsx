474e31a64f283e299ecaaae85351550ebc1b51ac
Tharindu Nimesh <70306612+lookatnim@users.noreply.github.com>
2024-03-23 22:51:41 +0530

    set up

---
+++ src/pages/EmployeeAdd/EmployeeAdd.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const employeeAdd = () => {
+  return (
+    <div>employeeAdd</div>
+  )
+}
+
+export default employeeAdd
---
+++ src/pages/EmployeeEdit/EmployeeEdit.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const employeeEdit = () => {
+  return (
+    <div>employeeEdit</div>
+  )
+}
+
+export default employeeEdit
